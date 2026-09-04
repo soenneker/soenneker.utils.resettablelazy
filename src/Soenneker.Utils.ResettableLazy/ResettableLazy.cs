f11b9628122dbcaff1612ba97fd33a2087e89f6f
soenneker <jake@soenneker.com>
2026-09-03 22:01:50 -0500

    Add interface inheritdoc references

--- src/Soenneker.Utils.ResettableLazy/ResettableLazy.cs
+++ src/Soenneker.Utils.ResettableLazy/ResettableLazy.cs
@@ -4,6 +4,7 @@ using Soenneker.Utils.ResettableLazy.Abstract;
 
 namespace Soenneker.Utils.ResettableLazy;
 
+/// <inheritdoc cref="IResettableLazy{T}" />
 public sealed class ResettableLazy<T> : IResettableLazy<T>
 {
     private Lazy<T> _lazy;
